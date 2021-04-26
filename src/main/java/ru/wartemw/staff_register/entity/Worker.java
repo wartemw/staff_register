b25d38531902d17c398000fb6b3edee3b76de637
wARTEMw_job <wartemw@gmail.com>
2021-04-26 10:49:27 +0700

    [hibernate][Worker] Added struct and create Gender enum file

--- src/main/java/ru/wartemw/staff_register/entity/Worker.java
+++ src/main/java/ru/wartemw/staff_register/entity/Worker.java
@@ -1,5 +1,32 @@
 package ru.wartemw.staff_register.entity;
 
+import java.util.Date;
+
+/*
+Фамилия                         String      surname
+Имя                             String      name
+отчество                        String      patronymic
+пол                             enum        gender
+дата рождения                   Data        date_of_birth
+должность                       String      position
+дата трудоустройства            Data        date_of_employment
+уровень зарплаты                Integer     salary_level
+номер рабочего телефона         Integer      work_number
+номер мобильного телефона       Integer      telephone_number
+банковсткая организация         String      banking_organization
+статус архивности               boolean     archival_status
+ */
 public class Worker {
-    
+    private String surname;                 // Фамилия
+    private String name;                    // Имя
+    private String patronymic;              // Отчество
+    private Gender gender;                  // Пол
+    private Date date_of_birth;             // Дата рождения
+    private String position;                // Должность
+    private Date date_of_employment;        // Дата трудоустройста
+    private Integer salary_level;           // уровень зарплаты
+    private Integer work_number;            // Номер рабочего телефона
+    private Integer telephone_number;       // Номер мобильного телефона
+    private String banking_organization;    // Банковская организация
+    private Boolean archival_status;        // Статус архивности
 }
